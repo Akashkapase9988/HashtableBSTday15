55a035905cc8f58780ab64e20b93615a0b9810b8
Akashkapase9988 <akashkapase9988@gmail.com>
2021-10-03 16:33:42 +0530

    [akash kapase] written program for hash table uc1 for count repetation of text

---
+++ uc1repetation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashandBSTday15
+{
+    class uc1repetation
+    {
+
+
+
+        public static int CountStringOccurrences(string text, string pattern)
+        {
+
+            int count = 0;
+            int i = 0;
+            while ((i = text.IndexOf(pattern, i)) != -1)
+            {
+                i += pattern.Length;
+                count++;
+            }
+            return (count);
+            }
+        }
+    }
+
